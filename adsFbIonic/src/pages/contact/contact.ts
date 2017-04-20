51d469c2b648120597d22301ced466ea4fccb53d
Khanh Van Dang <khanh.dang@vnhan-00100-mac.local>
2017-04-20 15:38:10 +0700

    Add test ngan luong request

--- adsFbIonic/src/pages/contact/contact.ts
+++ adsFbIonic/src/pages/contact/contact.ts
@@ -2,13 +2,20 @@ import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 import { Device } from 'ionic-native';
+import {Http,Headers, RequestOptions} from '@angular/http';
+import 'rxjs/Rx';
+
 @Component({
   selector: 'page-contact',
   templateUrl: 'contact.html'
 })
 export class ContactPage {
 	myUUID : any;
-  constructor(public navCtrl: NavController) {
+	carrierName: 'VIETTEL';
+	cardserinum : '';
+	cardnum : '';
+	listCarrier : ['VIETTEL','MOBI','VINA'];
+  constructor(public navCtrl: NavController,private http:Http) {
   	console.log(Device.uuid);
   	this.myUUID = Device.uuid;
   }
@@ -16,5 +23,36 @@ export class ContactPage {
   goToFacebookPage() : void {
   	let windowObjectReference;
     windowObjectReference = window.open("https://www.facebook.com/groups/ung.dung.quang.cao/","_system");
+  }
+  testSendCardSeries(){
+	var http2 = new XMLHttpRequest();
+	var url = 'https://www.nganluong.vn/mobile_card.api.post.v2.php?func=CardCharge&version=2.0&merchant_id=47851&merchant_account=vuducthanh2410@gmail.com&merchant_password=9025f3d087afc28cbcd8313726592ea9&pin_card='+this.cardnum+'&card_serial='+this.cardserinum+'&ref_code=ABC&type_card='+this.carrierName+'&client_fullname=123456&client_email=1234567&client_mobile='+this.myUUID;
+	console.log(url);
+    var params = "func=CardCharge&version=2.0&merchant_id=47851&merchant_account=vuducthanh2410@gmail.com&merchant_password=9025f3d087afc28cbcd8313726592ea9&pin_card=9548181232480&card_serial=57952002362&ref_code=ABC&type_card=VIETTEL&client_fullname=123456&client_email=1234567&client_mobile="+this.myUUID;
+
+    http2.open("POST", url, true);
+
+    http2.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+    http2.onreadystatechange = function() {
+        if (http2.readyState == 4 && http2.status == 200) {
+        	console.log(http2.responseText);
+        	var result = http2.responseText.substring(0,2);
+        	console.log(result);
+        	if(result == '00'){
+        		alert('Success!!');
+        	}
+        	else{
+        		alert('Error' + http2.responseText);
+        	}
+        }
+    }
+    http2.send(params);
+  }
+  sendCardNum(){
+  	console.log('send' + this.carrierName + this.cardserinum + this.cardnum);
+  	this.testSendCardSeries();
+  }
+  onChange(){
   }
 }
